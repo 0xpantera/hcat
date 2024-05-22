aa96b9c2ae61bd2f0a040ab784a01b2c65fffcda
0xpantera <0xpantera@proton.me>
2024-05-21 19:51:23 -0600

    finish pagination core

--- src/HCat.hs
+++ src/HCat.hs
@@ -4,6 +4,8 @@ module HCat where
 import qualified Control.Exception as Exception
 import qualified System.Environment as Env
 import qualified System.IO.Error as IOError
+import qualified System.Info as SystemInfo
+import           System.Process (readProcess)
 
 import qualified Data.ByteString as BS
 
@@ -45,3 +47,49 @@ groupsOf 0 _ = []
 groupsOf n xs =
     let (hd, tl) = splitAt n xs
     in hd : groupsOf n tl
+
+wordWrap :: Int -> T.Text -> [T.Text]
+wordWrap lineLen lineTxt
+    | T.length lineTxt <= lineLen = [lineTxt]
+    | otherwise =
+        let 
+            (candidate, nextLines) = T.splitAt lineLen lineTxt
+            (fstLine, overflow) = softWrap candidate (T.length candidate - 1)
+        in fstLine : wordWrap lineLen (overflow <> nextLines)
+    where
+        softWrap hardWrappedTxt txtIx
+            | txtIx <= 0 = (hardWrappedTxt, T.empty)
+            | T.index hardWrappedTxt txtIx == ' ' =
+                let (wrappedLine, rest) = T.splitAt txtIx hardWrappedTxt
+                in (wrappedLine, T.tail rest)
+            | otherwise = softWrap hardWrappedTxt (txtIx - 1)
+
+paginate :: ScreenDimensions -> T.Text -> [T.Text]
+paginate (ScreenDimensions rows cols) txt =
+    let
+        unwrappedLines = T.lines txt
+        wrappedLines = concatMap (wordWrap cols) unwrappedLines
+        pageLines = groupsOf rows wrappedLines
+    in map T.unlines pageLines
+
+data ScreenDimensions = ScreenDimensions
+    { screenRows :: Int
+    , screenCols :: Int
+    } deriving Show
+
+getTerminalSize :: IO ScreenDimensions
+getTerminalSize =
+    case SystemInfo.os of
+        "darwin" -> tputScreenDimensions
+        "linux" -> tputScreenDimensions
+        _other -> pure $ ScreenDimensions 25 80
+    where
+        tputScreenDimensions :: IO ScreenDimensions
+        tputScreenDimensions =
+            readProcess "tput" ["lines"] ""
+            >>= \lines ->
+                readProcess "tput" ["cols"] ""
+                >>= \cols ->
+                    let lines' = read $ init lines
+                        cols' = read $ init cols
+                    in return $ ScreenDimensions lines' cols'
